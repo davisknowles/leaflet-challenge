607804c2b31d3372ed062fe92aa22e546e96c3e3
Davis Knowles <dknowles52@gmail.com>
2021-04-19 19:26:09 -0400

    marker radius and color

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,16 +1,15 @@
-
 // Create the map object with options
 var map = L.map("mapid", {
     center: [34.0552, -118.2469],
-    zoom: 12
+    zoom: 6
 })
 
 // Adding tile layer
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
       attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-      maxZoom: 6,
+      maxZoom: 12,
       username: "dknowles52",
-      id: "mapbox/satellite-v9",
+      id: "mapbox/light-v10", //satellite-v9
       accessToken: API_KEY
 }).addTo(map);
 
@@ -20,17 +19,15 @@ var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.ge
 
 d3.json(url).then(function(data) {
     console.log(data.features[0]);
-    feature = data.features[0]
-    L.marker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]]).addTo(map);
-})
-
-d3.json(url).then(function(data) {
-    console.log(data.features[0]);
+    console.log(data.features[0].properties.mag)
     for (var i = 0; i < data.features.length; i++) {
         var feature = data.features[i];
-        if (feature) {
-            L.marker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]]).addTo(map);
-        }
+        var mag = [feature.properties.mag]
+        var depth = [feature.geometry.coordinates[2]]
+        L.circleMarker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], {
+            radius: mag * 5,
+            color: depth
+        }).addTo(map);
 
     }
 });
